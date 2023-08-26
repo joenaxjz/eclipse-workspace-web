19deaf3b6924e2efe6ce2fdc7e5d035ec171f3bd
jonashy <dta101203@gmail.com>
2023-08-26 12:37:31 +0700

    java web
    24/08/2023

--- DemoSession1_JSP_Serverlet_I2/src/main/java/com/demo/models/ProductModel.java
+++ DemoSession1_JSP_Serverlet_I2/src/main/java/com/demo/models/ProductModel.java
@@ -16,9 +16,9 @@ public class ProductModel {
 	
 	public static List<Product> findAll() {
 		List<Product> products = new ArrayList<Product>();
-		products.add(new Product(1, "Name 1", 5.6, 2, false, "pic1.jpg", new Category(1, "cate 1")));
-		products.add(new Product(2, "Name 2", 5.6, 2, false, "pic2.jpg", new Category(1, "cate 2")));
-		products.add(new Product(3, "Name 3", 5.6, 2, false, "pic3.jpg", new Category(1, "cate 3")));
+		products.add(new Product(1, "Tiv", 5.6, 2, false, "pic1.jpg", new Category(1, "cate 1")));
+		products.add(new Product(2, "Tu Lanh", 5.6, 2, false, "pic2.jpg", new Category(1, "cate 2")));
+		products.add(new Product(3, "Noi Com Dien", 5.6, 2, false, "pic3.jpg", new Category(1, "cate 3")));
 		return products;
 	}
 	
@@ -32,4 +32,14 @@ public class ProductModel {
 		return null;
 	} 
 	
+	public List<Product> searchByKeyword(String keyword){
+		List<Product> products = findAll();
+		List<Product> rs = new ArrayList<Product>();
+		for(Product product : products) {
+			if(product.getName().toUpperCase().contains(keyword.toUpperCase())) {
+				
+			}
+		}
+		return rs;
+	}
 }
